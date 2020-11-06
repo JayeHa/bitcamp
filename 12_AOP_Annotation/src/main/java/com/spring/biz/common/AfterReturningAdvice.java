b5bc2ee52e29de65b48a98a1e180b5f8bfe5c0de
JayeHa <jwha94@gmail.com>
2020-11-06 14:40:12 +0900

    afterReturningAdvice 어노테이션

--- 12_AOP_Annotation/src/main/java/com/spring/biz/common/AfterReturningAdvice.java
+++ 12_AOP_Annotation/src/main/java/com/spring/biz/common/AfterReturningAdvice.java
@@ -1,17 +1,25 @@
 package com.spring.biz.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
 import com.spring.biz.user.UserVO;
 
+@Service
+@Aspect
 public class AfterReturningAdvice {
-//	public void afterReturnLog() {
-//		System.out.println("[후처리-AfterReturningAdvice.afterReturnLog()]"
-//				+ " 비즈니스 로직 수행후 로그(정상실행시)");
-//	}
 
-	// returnObj : 추가 바인드 변수를 사용시에는 스프링 설정파일에 작성 추가
-	// 명시적으로 스프링 프레임워크에 매개변수 값을 받을 변수명 알림
+	//포인트컷 작성 : 명칭은 메소드명 사용
+	@Pointcut("execution(* com.spring.biz..*Impl.*(..))")
+	public void allPointcut() {}
+	
+	@Pointcut("execution(* com.spring.biz..*Impl.get*(..))")
+	public void getPointcut() {}
+	
+	@AfterReturning(pointcut = "getPointcut()", returning = "returnObj")
 	public void afterReturnLog(JoinPoint jp, Object returnObj) {
 		String methodName = jp.getSignature().getName();
 		

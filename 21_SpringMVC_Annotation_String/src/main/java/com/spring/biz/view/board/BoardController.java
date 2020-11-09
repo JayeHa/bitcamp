62eee86060a2cb2acf310dca3311d18978e45912
JayeHa <jwha94@gmail.com>
2020-11-09 17:22:32 +0900

    firstCommit

---
+++ 21_SpringMVC_Annotation_String/src/main/java/com/spring/biz/view/board/BoardController.java
@@ -0,0 +1,21 @@
+package com.spring.biz.view.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.biz.board.BoardVO;
+import com.spring.biz.board.impl.BoardDAO;
+
+@Controller
+public class BoardController {
+
+	@RequestMapping("/getBoard.do")
+	public ModelAndView getBoard(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+		BoardVO board = boardDAO.getBoard(vo);
+		mav.addObject("board", board);
+		mav.setViewName("getBoard.jsp");
+		return mav;
+	}
+	
+}

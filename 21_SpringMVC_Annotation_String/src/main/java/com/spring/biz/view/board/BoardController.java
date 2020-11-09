2ff96d616308850aa9fc290358fd0370ddb8900c
JayeHa <jwha94@gmail.com>
2020-11-09 17:27:18 +0900

    여러개의 컨트롤러를 BoardController로 합쳤음

--- 21_SpringMVC_Annotation_String/src/main/java/com/spring/biz/view/board/BoardController.java
+++ 21_SpringMVC_Annotation_String/src/main/java/com/spring/biz/view/board/BoardController.java
@@ -1,5 +1,7 @@
 package com.spring.biz.view.board;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,4 +20,38 @@ public class BoardController {
 		return mav;
 	}
 	
+	@RequestMapping("/getBoardList")
+	public ModelAndView getBoardList(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+		System.out.println(">>> 게시글 전체 목록 보여주기 - getBoardList()");
+		
+		List<BoardVO> list = boardDAO.getBoardList(vo);
+		mav.addObject("boardList", list);
+		mav.setViewName("getBoardList.jsp");
+		
+		return mav;
+	}
+
+	@RequestMapping("/insertBoard.do")
+	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
+		System.out.println(">>> 게시글 입력 처리 - insertBoard()");
+		boardDAO.insertBoard(vo);
+		
+		return "redirect:getBoardList.do";
+	}	
+	
+	@RequestMapping("/updateBoard.do")
+	public String updateBoard(BoardVO vo, BoardDAO boardDAO) {
+		System.out.println(">>> 게시글 수정 처리");
+		
+		boardDAO.updateBoard(vo);
+		
+		return "redirect:getBoardList.do";
+	}	
+	
+	@RequestMapping("/deleteBoard.do")
+	public String deleteBoard(BoardVO vo, BoardDAO boardDAO) {
+		System.out.println(">>> 게시글 삭제 처리");
+		boardDAO.deleteBoard(vo);
+		return "redirect:getBoardList.do";
+	}	
 }

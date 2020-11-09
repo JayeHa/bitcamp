cd3d51e2d6aeb7edee0d2bfa1d1246176fe5e722
JayeHa <jwha94@gmail.com>
2020-11-09 15:52:54 +0900

    로그아웃 컨트롤러

--- 20_SpringMVC_Annotation/src/main/java/com/spring/biz/view/user/LogoutController.java
+++ 20_SpringMVC_Annotation/src/main/java/com/spring/biz/view/user/LogoutController.java
@@ -1,29 +1,19 @@
 package com.spring.biz.view.user;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.spring.biz.user.UserVO;
-import com.spring.biz.user.impl.UserDAO;
+@Controller
+public class LogoutController {
 
-public class LogoutController implements Controller {
-
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		System.out.println(">>> 로그아웃 처리");
+	@RequestMapping("/logout.do")
+	public String logout(HttpSession session) {
 		//1. 세션 초기화
-		request.getSession().invalidate();
+		session.invalidate();
 		
-		//2. 화면 네비게이션(로그인페이지)		
-		return new ModelAndView("login.jsp");
+		return "login.jsp";
 	}
 
 }
-
-
-
-
-

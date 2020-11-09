c7e5c3ef33138147f7760d82b91012acd80f0b01
JayeHa <jwha94@gmail.com>
2020-11-09 15:58:23 +0900

    getBoardList

--- 20_SpringMVC_Annotation/src/main/java/com/spring/biz/view/board/GetBoardListController.java
+++ 20_SpringMVC_Annotation/src/main/java/com/spring/biz/view/board/GetBoardListController.java
@@ -5,15 +5,30 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import com.spring.biz.board.BoardVO;
 import com.spring.biz.board.impl.BoardDAO;
 
-public class GetBoardListController implements Controller {
+@Controller
+public class GetBoardListController {
 
-	@Override
+	@RequestMapping("/getBoardList")
+	public ModelAndView getBoardList(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+		System.out.println(">>> 게시글 전체 목록 보여주기 - getBoardList()");
+
+		List<BoardVO> list = boardDAO.getBoardList(vo);
+		
+		mav.addObject("boardList", list);
+		mav.setViewName("getBoardList.jsp");
+		
+		return mav;
+
+	}
+
+	/*
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println(">>> 게시글 전체 목록 보여주기");
 		BoardVO vo = new BoardVO();
@@ -32,7 +47,7 @@ public class GetBoardListController implements Controller {
 		
 		return mav;
 	}
-
+*/
 }
 
 
